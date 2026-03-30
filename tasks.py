c524caa5b5eb560da1cd787696b811577a85e735
Pii Sainio <pii.sainio@helsinki.fi>
2026-03-30 15:10:53 +0300

    Add Invoke.

---
+++ tasks.py
@@ -0,0 +1,33 @@
+from invoke import task
+from subprocess import call
+from sys import platform
+
+@task
+def start(ctx):
+    if platform == "win32":
+        ctx.run("python3 src/index.py")
+    else:
+        ctx.run("python3 src/index.py", pty=True)
+
+@task
+def test(ctx):
+    if platform == "win32":
+        ctx.run("pytest src")
+    else:
+        ctx.run("pytest src", pty=True)
+
+@task
+def coverage(ctx):
+    if platform == "win32":
+        ctx.run("coverage run --branch -m pytest src")
+    else:
+        ctx.run("coverage run --branch -m pytest src", pty=True)
+
+@task(coverage)
+def coverage_report(ctx):
+    if platform == "win32":
+        ctx.run("coverage html")
+        ctx.run("start htmlcov/index.html")
+    else:
+        ctx.run("coverage html", pty=True)
+        call(("xdg-open", "htmlcov/index.html"))
